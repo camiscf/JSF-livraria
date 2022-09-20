fd654cc4837e1aeed6a85824fd5704847d70be63
Camila <camilacaleones@dcc.ufrj.br>
2022-09-20 11:36:45 -0300

    Initial commit

---
+++ src/br/com/livraria/bean/LivroBean.java
@@ -0,0 +1,20 @@
+package br.com.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+	
+	public Livro getLivro() {
+		return livro;
+	}
+	
+	
+	public void gravar() {
+		System.out.println("Gravando livro: " + this.livro.titulo);
+	}
+
+
+}
